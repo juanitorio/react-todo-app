729e5afa6ca8e7314bf80b5199defbd62ad39634
Sebastian T F <sebastian.tharakan97@gmail.com>
2019-07-14 20:28:19 +0530

    destructure todo in Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 
 export default class Todo extends Component {
     render() {
+        const { id, task, completed } = this.props.todo;
         return (
             <li>
-                <label style={{ textDecoration: this.props.todo.completed ? "line-through" : "none" }}>
-                    <input type="checkbox" onChange={this.props.markCompleted.bind(this, this.props.todo.id)} /> {this.props.todo.task}
+                <label style={{ textDecoration: completed ? "line-through" : "none" }}>
+                    <input type="checkbox" onChange={this.props.markCompleted.bind(this, id)} /> {task}
                 </label>
             </li>
         );
