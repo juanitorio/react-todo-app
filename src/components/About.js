28efdea57ac8cb7656a06a23c1abbf44001bd00e
Sebastian T F <sebastian.tharakan97@gmail.com>
2019-07-14 23:31:52 +0530

    create About component

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h3>About Us</h3>
+            <p>This is the about page of the react-todo app.</p>
+        </React.Fragment>
+    );
+}
