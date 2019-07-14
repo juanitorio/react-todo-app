df3143c0405bed3a2051c1f3b44574819df984e6
Sebastian T F <sebastian.tharakan97@gmail.com>
2019-07-14 14:58:47 +0530

    create TodoList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import TodoList from "./components/TodoList";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
             <div className="App-header">
-            <Header />
+                <Header />
+                <TodoList />
             </div>
         </div>
     );
---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <ul className="todo-list">
+                <li>Todo 1</li>
+                <li>Todo 2</li>
+            </ul>
+        );
+    }
+}
