ff1a3f97de84b43347c5fd20f8a525f0eb114f9f
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-04-06 15:04:01 +0530

    Updates

---
+++ precache-manifest.374f4bda4a5c14e9f5e4416ed5d04e89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15cfea5031a5c6e4dc81480b9d5dcd8",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "7856ab1a080c56762fdb",
+    "url": "/react-todo-app/static/css/main.7645b3ab.chunk.css"
+  },
+  {
+    "revision": "bc0b4039259ee435c920",
+    "url": "/react-todo-app/static/js/2.c3309461.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.c3309461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7856ab1a080c56762fdb",
+    "url": "/react-todo-app/static/js/main.0845c2f0.chunk.js"
+  },
+  {
+    "revision": "84aed2b3df399991f155",
+    "url": "/react-todo-app/static/js/runtime-main.63a23138.js"
+  }
+]);
