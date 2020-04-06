a780e3d82f7e3d2609f39edd4d4e5b45f772fad0
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-04-06 15:44:51 +0530

    Updates

--- precache-manifest.374f4bda4a5c14e9f5e4416ed5d04e89.js
+++ precache-manifest.374f4bda4a5c14e9f5e4416ed5d04e89.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fc86db8d0b4d95c10818138a2413c871",
+    "revision": "a15cfea5031a5c6e4dc81480b9d5dcd8",
     "url": "/react-todo-app/index.html"
   },
   {
-    "revision": "e73ecf85c40f92ed548a",
+    "revision": "7856ab1a080c56762fdb",
     "url": "/react-todo-app/static/css/main.7645b3ab.chunk.css"
   },
   {
-    "revision": "da851fbdd4e6460d9d95",
-    "url": "/react-todo-app/static/js/2.88e7eb5a.chunk.js"
+    "revision": "bc0b4039259ee435c920",
+    "url": "/react-todo-app/static/js/2.c3309461.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-todo-app/static/js/2.88e7eb5a.chunk.js.LICENSE.txt"
+    "url": "/react-todo-app/static/js/2.c3309461.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e73ecf85c40f92ed548a",
-    "url": "/react-todo-app/static/js/main.4c3579e3.chunk.js"
+    "revision": "7856ab1a080c56762fdb",
+    "url": "/react-todo-app/static/js/main.0845c2f0.chunk.js"
   },
   {
     "revision": "84aed2b3df399991f155",
